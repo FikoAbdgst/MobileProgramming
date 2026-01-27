22e748a256bd37bf74a650e2de816ef58a8558d0
fikoabdgst <fikoabdigusti15@gmail.com>
2026-01-27 08:44:26 +0700

    start commit project

---
+++ UASEcom/app/src/main/java/com/example/uasecom/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.example.uasecom
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.uasecom.data.GoogleAuthUIClient
+import com.example.uasecom.presentation.profile.ProfileScreen
+import com.example.uasecom.presentation.sign_in.SignInScreen
+import com.example.uasecom.presentation.sign_in.SignInViewModel
+import com.example.uasecom.ui.theme.UASEcomTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    private val googleAuthUIClient by lazy {
+        GoogleAuthUIClient(
+            context = this
+        )
+    }
+    @SuppressLint("ViewModelConstructorInComposable")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UASEcomTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+
+                    NavHost( navController = navController, startDestination = "sign_in"){
+                        composable("sign_in"){
+                            val viewModel = SignInViewModel()
+                            val state by viewModel.state.collectAsStateWithLifecycle()
+
+                            LaunchedEffect(key1 = Unit) {
+                                if (googleAuthUIClient.getSignedInUser() != null) {
+                                    navController.navigate("profile")
+                                }
+                            }
+                            LaunchedEffect(key1 = state.isSignInSuccessful) {
+                                if (state.isSignInSuccessful){
+                                    Toast.makeText(
+                                        applicationContext,
+                                        "Sign in successful",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+
+                                    navController.navigate("profile")
+                                    viewModel.resetState()
+                                }
+                            }
+                            SignInScreen(
+                                state = state,
+                                onSignInClick = {
+                                    lifecycleScope.launch {
+                                        val result = googleAuthUIClient.signIn()
+                                        viewModel.onSignInResult(result)
+                                    }
+                                }
+                            )
+                        }
+                        composable("profile") {
+                            ProfileScreen(
+                                userData = googleAuthUIClient.getSignedInUser(),
+                                onSignOut = {
+                                    lifecycleScope.launch {
+                                        googleAuthUIClient.signOut()
+                                        Toast.makeText(
+                                            applicationContext,
+                                            "Signed out",
+                                            Toast.LENGTH_LONG
+                                        ).show()
+                                        navController.popBackStack()
+                                    }
+                                }
+                            )
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
+
